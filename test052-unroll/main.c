5ee45b91aa626a0a2c4ac396159b9b0a2c6e046a
snsinfu <snsinfu@gmail.com>
2020-07-30 00:29:48 +0900

    test052: Came up with an algorithm. Almost solved!

--- test052-unroll/main.c
+++ test052-unroll/main.c
@@ -74,8 +74,21 @@
 //   r(4) - x(5) = x(3) + x(4)
 //   r(5) - x(5) =        x(4) + x(6)
 //
-// I need x(6).
+// I need x(6). Similarly, to get x(6) I need x(9), which requires x(12), which
+// requires x(15), and so on. So I just need to brute force x(3p) where p is the
+// maximum integer that satisfies 3p < N.
+
+// Algorithm. Firstly, solve for x(3k-1) where k = 1,2,3,..., Then, bruteforce
+// on x(3p) to get:
 //
+//   x(3p-2) = r(3p-1) - x(3p-1) - x(3p)
+//   x(3p-3) = r(3p-2) - x(3p-1) - x(3p-2)
+//
+// Note that x(3p-1) is already solved. So, we get x(3p-2) and x(3p-3). Next,
+// let p := p-1 and repeat the same procedure to obtain all x values.
+//
+
+// Lastly, I need to get x(i) for i > 3p.
 
 
 int main()
@@ -100,6 +113,15 @@ int main()
     for (size_t i = 2; i < size; i += 3) {
         x = rolling_sums[i - 1] - rolling_sums[i - 2] + x;
         solution[i] = x;
+    }
+
+    size_t const p = (size - 1) / 3 * 3;
+
+    solution[p] = 0; // assumption
+
+    for (size_t q = p; q >= 3; q -= 3) {
+        solution[q - 2] = rolling_sums[q - 1] - solution[q - 1] - solution[q];
+        solution[q - 3] = rolling_sums[q - 2] - solution[q - 2] - solution[q - 1];
     }
 
     puts("i\ttruth\tsolution");

7e87dbd3c69363603ab6e3afd1c311ea86fb6ce0
snsinfu <snsinfu@gmail.com>
2020-08-01 18:53:12 +0900

    test054: TODO: Framebuffer...

--- test054-fragment_shader_2d_demo/main.go
+++ test054-fragment_shader_2d_demo/main.go
@@ -52,6 +52,65 @@ func run() error {
 		return err
 	}
 
+	// Surface quad. We directly feed a 2D quad vertices defined in the viewport
+	// coordinates so we can omit vertex shader.
+	// TODO.
+
+	vertexCoords := []float32{
+		-1, -1, 0,
+		1, -1, 0,
+		-1, 1, 0,
+		1, 1, 0,
+	}
+
+	vertexTriangles := []uint32{
+		0, 1, 3,
+		0, 2, 3,
+	}
+
+	_ = vertexCoords
+	_ = vertexTriangles
+
+	// Surface framebuffer and texture.
+	//
+	// TODO: I want to allow fragment shader to read previous pixels to compute
+	// new pixel values. Maybe I need to use two buffers and swap these on each
+	// frame.
+
+	var surfaceFBO uint32
+	gl.GenFramebuffers(1, &surfaceFBO)
+	gl.BindFramebuffer(gl.FRAMEBUFFER, surfaceFBO)
+
+	var surfaceTex uint32
+	gl.GenTextures(1, &surfaceTex)
+	gl.BindTexture(gl.TEXTURE_2D, surfaceTex)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, windowWidth, windowHeight, 0, gl.RGB, gl.UNSIGNED_BYTE, nil)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+
+	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, surfaceTex, 0)
+
+	if gl.CheckFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE {
+		return fmt.Errorf("framebuffer not complete")
+	}
+
+	// Fragment shader.
+	// TODO: Load from file.
+
+	fragShader := `
+#version 330 core
+
+uniform sampler2D surface;
+
+void main() {
+	gl_FragColor = texture(surface, gl_FragCoords.xy);
+}
+`
+
+	_ = fragShader
+
+	// Main loop.
+
 	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
 
 	for !window.ShouldClose() {

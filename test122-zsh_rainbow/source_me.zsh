eefb37764037f97459a1553b6cfb74677d872686
snsinfu <snsinfu@gmail.com>
2020-10-18 03:22:25 +0900

    test122: Make rainbow smoother

--- test122-zsh_rainbow/source_me.zsh
+++ test122-zsh_rainbow/source_me.zsh
@@ -1,9 +1,17 @@
 zmodload zsh/sched
 
-rainbow_palette=(196 214 226 154 46 49 51 39 21 129 201 199)
+rainbow_palette=(
+    196 202 208 214 220 226 226 190 154 82 46 46 47 48 49
+    50 51 45 39 33 27 21 21 57 129 165 201 201 200 199 198 197
+)
 rainbow_offset=0
 
-rainbow_color() {
+rainbow_color:inc() {
+    local n=${#rainbow_palette}
+    echo -n ${rainbow_palette[1 + ($1 + rainbow_offset - 1) % n]}
+}
+
+rainbow_color:dec() {
     local n=${#rainbow_palette}
     local i=$((n - rainbow_offset % n + $1))
     echo -n ${rainbow_palette[1 + (i - 1) % n]}
@@ -14,15 +22,16 @@ rainbow_prompt() {
     local chars=( ${(s::)${(%)text}} )
     local deco_prompt=
     local i
+    local n=${#rainbow_palette}
 
-    for ((i = 1; i <= ${#rainbow_palette}; i++)); do
-        deco_prompt+="%K{$(rainbow_color ${i})} "
+    for ((i = n / 2; i <= n; i++)); do
+        deco_prompt+="%K{$(rainbow_color:dec ${i})} "
     done
 
     deco_prompt+="%K{reset} "
 
     for ((i = 1; i <= ${#chars}; i++)); do
-        deco_prompt+="%F{$(rainbow_color ${i})}"
+        deco_prompt+="%F{$(rainbow_color:dec ${i})}"
         deco_prompt+=${chars[i]}
     done
 

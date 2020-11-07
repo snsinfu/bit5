6230fac0138e65870a37491f6ccc01f2bb4a314b
snsinfu <snsinfu@gmail.com>
2020-11-07 20:32:05 +0900

    test141: Now it does not crash but tree is incorrect

--- test141-23_tree/main.py
+++ test141-23_tree/main.py
@@ -15,7 +15,15 @@ def main():
     root = insert(root, 60)
     root = insert(root, 40)
     root = insert(root, 80)
-    #root = insert(root, 90) # FIXME
+    root = insert(root, 90)
+
+    root = insert(root, 35)
+    root = insert(root, 15)
+    root = insert(root, 75)
+    root = insert(root, 65)
+    root = insert(root, 45)
+    root = insert(root, 85)
+    root = insert(root, 95)
 
     walk(root)
 
@@ -44,28 +52,111 @@ def do_insert(node, value):
     if len(node.values) == 2:
         return node
 
+    # Split the node into two sibling nodes having lower and upper key.
+    # Median key is promoted to the parent node. This can make the parent
+    # node over-keyed (i.e., having three keys). We need to fix that.
     lower, median, upper = node.values
+    parent = node.parent
 
-    sibling = Node(lower, node.parent)
+    lower_sibling = Node(lower, parent)
+    upper_sibling = Node(upper, parent)
+
+    if parent is None:
+        parent = Node(median)
+        parent.children = [lower_sibling, upper_sibling]
+        lower_sibling.parent = parent
+        upper_sibling.parent = parent
+        return parent
+
+    if len(parent.children) == 2:
+        # OK:
+        #   p           p
+        #  [k]        [k,m]
+        #  / \   =>   / | \
+        # *   n      *  s  n
+        v, = parent.values
+        a, b = parent.children
+        if a is node:
+            parent.values = [median, v]
+            parent.children = [lower_sibling, upper_sibling, b]
+        else:
+            parent.values = [v, median]
+            parent.children = [a, lower_sibling, upper_sibling]
+    else:
+        # Fix:
+        #                                   |
+        #                                   p
+        #    |             |               [k]
+        #    p             p              /   \
+        #  [h,k]        [h,k,m]         [h]   [m]
+        #  / | \   =>   / | | \    =>   / \   / \
+        # *  *  n      *  * s  n       *   * s   n
+        u, v = parent.values
+        a, b, c = parent.children
+        if a is node:
+            proxy_1 = Node(median, parent)
+            proxy_2 = Node(v, parent)
+            parent.values = [u]
+            parent.children = [proxy_1, proxy_2]
+            proxy_1.children = [lower_sibling, upper_sibling]
+            proxy_2.children = [b, c]
+        elif b is node:
+            proxy_1 = Node(u, parent)
+            proxy_2 = Node(v, parent)
+            parent.values = [median]
+            parent.children = [proxy_1, proxy_2]
+            proxy_1.children = [a, lower_sibling]
+            proxy_2.children = [upper_sibling, c]
+        else:
+            proxy_1 = Node(u, parent)
+            proxy_2 = Node(median, parent)
+            parent.values = [v]
+            parent.children = [proxy_1, proxy_2]
+            proxy_1.children = [a, b]
+            proxy_2.children = [lower_sibling, upper_sibling]
+
+    return parent
+
+
+def split(node):
+    """
+    Split over-keyed node.
+    """
+    assert len(node.values) == 3
+
+    lower, median, upper = node.values
+    left, middle, right = node.children
+
+    sibling = Node(lower, parent)
     node.values = [upper]
+
+    sibling.children = [left]
+    node.children = [right]
 
     if node.parent is None:
         parent = Node(median)
         parent.children = [sibling, node]
-        sibling.parent = parent
-        node.parent = parent
+        sibling.parent = node.parent = parent
         return parent
 
     if len(node.parent.children) == 1:
         node.parent.children = [sibling, node]
+        node.parent.values.append(median)
+        node.parent.values.sort()
     else:
         a, b = node.parent.children
         if a is node:
             node.parent.children = [sibling, node, b]
         else:
             node.parent.children = [a, sibling, node]
+        node.parent.values.append(median)
+        node.parent.values.sort()
 
-    return do_insert(node.parent, median)
+
+    if len(node.parent.values) == 3:
+        split(node.parent)
+
+    return node.parent
 
 
 def find_leaf(node, value):

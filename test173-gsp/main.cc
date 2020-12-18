4ed6610f520157454d59b02697e4133645e57fb5
snsinfu <snsinfu@gmail.com>
2020-12-18 14:55:17 +0900

    test173: Make reaction network more complex

--- test173-gsp/main.cc
+++ test173-gsp/main.cc
@@ -9,18 +9,44 @@ int main()
 {
     long const simulation_steps = 10'000;
     std::uint64_t const random_seed = 0;
-
-    gsp::simulation sim({.species = {0, 0, 0}});
-
-    // (nothing) --> A --> B --> C --> (nothing)
-    sim.add_reaction(gsp::simple_generation({.species = 0, .rate = 0.1}));
-    sim.add_reaction(gsp::simple_transformation({.reactant = 0, .product = 1, .rate = 0.1}));
-    sim.add_reaction(gsp::simple_transformation({.reactant = 1, .product = 2, .rate = 0.1}));
-    sim.add_reaction(gsp::simple_decay({.species = 2, .rate = 0.1}));
-
-    std::cout << sim.state() << '\n';
+    std::size_t const species_count = 30;
 
     std::mt19937_64 random(random_seed);
+
+    gsp::state init_state;
+    init_state.species.resize(species_count);
+    {
+        std::uniform_int_distribution<int> count(0, 10);
+        std::generate(init_state.species.begin(), init_state.species.end(), [&] {
+            return count(random);
+        });
+    }
+
+    gsp::simulation sim(init_state);
+
+    for (std::size_t i = 0; i < species_count; i++) {
+        std::uniform_int_distribution<std::size_t> species(0, species_count);
+        std::uniform_real_distribution<double> rate(0.1, 1.0);
+
+        auto const a = i;
+        auto const b = species(random);
+
+        sim.add_reaction(
+            gsp::simple_transformation({
+                .reactant = a,
+                .product = b,
+                .rate = rate(random),
+            })
+        );
+
+        sim.add_reaction(
+            gsp::simple_transformation({
+                .reactant = b,
+                .product = a,
+                .rate = rate(random),
+            })
+        );
+    }
 
     for (long step = 1; step <= simulation_steps; step++) {
         auto const reaction_id = sim.step(random);

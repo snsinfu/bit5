891cfc56cb334ea020ffd6e3d147db93eda435bf
snsinfu <snsinfu@gmail.com>
2021-06-24 02:55:23 +0900

    test346: cmake header-only + example

--- test346-cmake/example/main.cpp
+++ test346-cmake/example/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+
+#include <mylib.hpp>
+
+
 int main()
 {
+    std::cout << mylib::major_version << '\n';
 }
--- test346-cmake/include/mylib.hpp
+++ test346-cmake/include/mylib.hpp
@@ -1,4 +1,9 @@
 #ifndef INCLUDED_MYLIB_HPP
 #define INCLUDED_MYLIB_HPP
 
+namespace mylib
+{
+    inline constexpr int major_version = 1;
+}
+
 #endif

e4184eb5f736912ae61c065efbf8da0baf127d3a
snsinfu <snsinfu@gmail.com>
2021-06-24 03:49:17 +0900

    test346,348: cmake embedding header-only library

---
+++ test346-cmake/cmake/test346-config.cmake
@@ -0,0 +1,4 @@
+cmake_minimum_required(VERSION 3.10)
+
+add_library(test346 INTERFACE)
+target_include_directories(test346 INTERFACE "${CMAKE_CURRENT_LIST_DIR}/../include")
---
+++ test348-cmake_module/main.cpp
@@ -0,0 +1,6 @@
+#include <mylib.hpp>
+
+int main()
+{
+    return mylib::major_version == 1 ? 0 : 1;
+}

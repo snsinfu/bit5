d28ba85073732240690e2e1949a726e8532106e3
snsinfu <snsinfu@gmail.com>
2021-08-18 02:30:51 +0900

    Add test391: SWT (FIXME)

---
+++ test391-swt/app/src/main/kotlin/demo/App.kt
@@ -0,0 +1,17 @@
+package demo
+
+fun main() {
+    val display = Display()
+    val shell = Shell(display)
+
+    shell.open()
+
+    while (!shell.isDisposed) {
+        if (!display.readAndDispatch()) {
+            display.sleep()
+        }
+    }
+
+    display.dispose()
+
+}

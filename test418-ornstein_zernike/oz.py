853b18b238588a8a804dad8fd48ee4bdf69eb320
snsinfu <snsinfu@gmail.com>
2021-09-17 17:37:58 +0900

    test418: Fix OZ inversion procedure
    I found a typo in the integral equation shown in Behrens (2001).
    Fixed that. Also refactored kernel integration to a matrix form.

--- test418-ornstein_zernike/oz.py
+++ test418-ornstein_zernike/oz.py
@@ -13,27 +13,10 @@ def main():
     corrector = np.zeros(n_bins)
     kernel = compute_kernel(rdf, bin_width)
 
-    @numba.njit
-    def estimate_corrector(r_bin):
-        integral = 0
-        for s_bin in range(n_bins):
-            s = (s_bin + 0.5) * bin_width
-            integral += (
-                (rdf[r_bin] - 1 - density * corrector[s_bin])
-                * kernel[r_bin, s_bin]
-                * s**2
-                * bin_width
-            )
-        return integral
+    for step in range(100):
+        corrector = bin_width * kernel @ (rdf - 1 - density * corrector)
 
-    next_corrector = np.zeros(n_bins)
-
-    for _ in range(100):
-        for r_bin in range(n_bins):
-            next_corrector[r_bin] = estimate_corrector(r_bin)
-        corrector[:] = next_corrector
-
-    direct_rdf = rdf * np.exp(density * corrector)
+    direct_rdf = np.exp(np.log(rdf) - density * corrector)
 
     for r, raw_gr, direct_gr in zip(starts, rdf, direct_rdf):
         print(f"{r:.3f}\t{raw_gr:.3f}\t{direct_gr:.3f}")
@@ -46,7 +29,6 @@ def compute_kernel(rdf, bin_width):
     @numba.njit
     def integrate(r, s, div=1000):
         theta_step = math.pi / div
-
         integral = 0
 
         for theta_bin in range(div):
@@ -56,7 +38,7 @@ def compute_kernel(rdf, bin_width):
             if distance_bin < n_bins:
                 integral += (rdf[distance_bin] - 1) * math.sin(theta)
 
-        integral *= 2 * math.pi * theta_step
+        integral *= 2 * math.pi * s**2 * theta_step
 
         return integral
 

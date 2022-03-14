f45ae338ac63c20dd94551d11fc7cc3f025dd971
snsinfu <snsinfu@gmail.com>
2022-03-15 01:35:22 +0900

    test433: Add JAX notes

--- test433-jaxloop/main.py
+++ test433-jaxloop/main.py
@@ -1,23 +1,27 @@
 import jax
 import jax.numpy as jnp
-import numpy as np
 
 
 def main():
     def loss(x):
-        return jnp.mean(((x - 1) * (x + 1)) ** 2)
+        return jnp.mean(jnp.square((x - 1) * (x + 1)))
 
-    init = jnp.array(np.random.normal(0, 1, size=10))
-    x, history = gradient_descent(loss, init, 0.01, decay_rate=0.1)
+    key = jax.random.PRNGKey(0)
+    init = jax.random.normal(key, shape=(10,))
+    x, history = gradient_descent(loss, init, rate=1, max_iter=500, tol=1e-10)
+
+    print(history)
     print(x)
 
 
-# @jax.jit
-def gradient_descent(loss_func, init, rate, *, decay_rate=0, max_iter=100):
+# Note: JAX cannot JIT a function whose parameters include a function.
+
+def gradient_descent(loss_func, init, rate, *, decay_rate=0, max_iter=100, tol=0):
     loss_grad = jax.value_and_grad(loss_func)
 
     def cond_func(state):
-        return state["step"] <= max_iter
+        # Note: "and" operator trips up JAX. "&" operator works.
+        return (state["step"] <= max_iter) & ~(state["delta"] < tol)
 
     def loop_func(state):
         step = state["step"]
@@ -27,24 +31,28 @@ def gradient_descent(loss_func, init, rate, *, decay_rate=0, max_iter=100):
         loss, grad = loss_grad(x)
         cur_rate = rate / (1 + decay_rate * step)
         dx = cur_rate * grad
+        delta = jnp.max(jnp.abs(dx))
 
         state = {
             "step":    step + 1,
-            "x":       x + dx,
+            "delta":   delta,
+            "x":       x - dx,
             "history": history.at[step].set(loss),
         }
         return state
 
     state = {
         "step":    0,
+        "delta":   jnp.nan,
         "x":       jnp.asarray(init),
         "history": jnp.full(max_iter, jnp.nan),
     }
     state = jax.lax.while_loop(cond_func, loop_func, state)
 
-    return state["x"], state["history"]
+    x = state["x"]
+    history = state["history"][:state["step"]]
+    return x, history
 
 
 if __name__ == "__main__":
     main()
-main()
